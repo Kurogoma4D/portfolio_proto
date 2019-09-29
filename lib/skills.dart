176e7e46b24fe3ecca2bbf8c0c38c8725d8e7f16
Takafumi Suzuki <highdro0479@gmail.com>
2019-09-29 17:29:02 +0900

    modify: skills

--- lib/skills.dart
+++ lib/skills.dart
@@ -6,7 +6,11 @@ class _Props {
     fontSize: 24,
     color: Colors.white,
   );
-
+  static TextStyle subtitleStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+    color: Colors.white,
+  );
   static TextStyle skillNameStyle = TextStyle(
     fontSize: 14,
   );
@@ -43,6 +47,20 @@ class _Props {
           "高専一年次より講義で使用．プログラミングの基礎，アルゴリズム，データ構造，組み込みプログラミング，ニューラルネットワークなど",
     },
   ];
+  static List<Map<String, String>> designSkills = [
+    {
+      "name": "Cinema4D",
+      "url": "https://img.icons8.com/ios/100/000000/programming.png",
+      "description":
+          "3DCG制作のときにメインで使用するツール．Octane Renderを使用したフォトリアル レンダリングを主に行う．Pythonを用いた外部連携も可能．",
+    },
+    {
+      "name": "Adobe CC",
+      "url": "https://img.icons8.com/color/144/000000/adobe-creative-cloud.png",
+      "description":
+          "よく使用するツール：Photoshop，Xd，Lightroom\n\n使用できるツール：Illustrator，Indesign，AfterEffects",
+    },
+  ];
 
   static List<String> poorSkills = [
     "C#",
@@ -65,7 +83,12 @@ class Skills extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text("プログラミング - そこそこできる", style: _Props.titleStyle),
+          RichText(
+            text: TextSpan(children: [
+              TextSpan(text: "プログラミング - ", style: _Props.titleStyle),
+              TextSpan(text: "そこそこできる", style: _Props.subtitleStyle),
+            ]),
+          ),
           const Divider(thickness: 1, height: 1.6, color: Colors.white54),
           const SizedBox(height: 16),
           GridView.count(
@@ -73,11 +96,16 @@ class Skills extends StatelessWidget {
             crossAxisCount: 5,
             crossAxisSpacing: 12,
             shrinkWrap: true,
-            children:
-                List.generate(4, (index) => _buildGridItem(index, context)),
+            children: List.generate(
+                4, (index) => _buildGridItem(_Props.skills[index], context)),
           ),
           const SizedBox(height: 42),
-          Text("プログラミング - さわったことある", style: _Props.titleStyle),
+          RichText(
+            text: TextSpan(children: [
+              TextSpan(text: "プログラミング - ", style: _Props.titleStyle),
+              TextSpan(text: "さわったことある", style: _Props.subtitleStyle),
+            ]),
+          ),
           const Divider(thickness: 1, height: 1.6, color: Colors.white54),
           const SizedBox(height: 16),
           Wrap(
@@ -90,30 +118,42 @@ class Skills extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(height: 42),
+          Text("デザイン", style: _Props.titleStyle),
+          const Divider(thickness: 1, height: 1.6, color: Colors.white54),
+          const SizedBox(height: 16),
+          GridView.count(
+            physics: NeverScrollableScrollPhysics(),
+            crossAxisCount: 5,
+            crossAxisSpacing: 12,
+            shrinkWrap: true,
+            children: List.generate(2,
+                (index) => _buildGridItem(_Props.designSkills[index], context)),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildGridItem(int index, BuildContext context) {
+  Widget _buildGridItem(Map<String, String> skill, BuildContext context) {
     return GestureDetector(
       child: Card(
         child: Padding(
           padding: const EdgeInsets.all(8),
-          child: Image.network(_Props.skills[index]["url"]),
+          child: Image.network(skill["url"]),
         ),
       ),
-      onTap: () => _onPressed(index, context),
+      onTap: () => _onPressed(skill, context),
     );
   }
 
-  void _onPressed(int index, BuildContext context) async {
+  void _onPressed(Map<String, String> skill, BuildContext context) async {
     await showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text(_Props.skills[index]["name"]),
-            content: Text(_Props.skills[index]["description"]),
+            title: Text(skill["name"]),
+            content: Text(skill["description"]),
           );
         });
   }
